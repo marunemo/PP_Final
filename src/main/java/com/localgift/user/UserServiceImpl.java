07ad631dccd2344fd28fa6d36a00db36da3e6df8
marunemo <chys3697@gmail.com>
2021-12-12 03:38:22 +0900

    create: UserServiceImpl

--- src/main/java/com/localgift/user/UserServiceImpl.java
+++ src/main/java/com/localgift/user/UserServiceImpl.java
@@ -1,5 +1,14 @@
 package com.localgift.user;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserServiceImpl {
+	@Autowired
+	UserDAO userDAO;
+	
+	public UserVO getUser(UserVO vo) {
+		return userDAO.getUser(vo);
+	}
 }
