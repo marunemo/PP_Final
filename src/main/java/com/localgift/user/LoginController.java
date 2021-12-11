a2b6632bcc6a2ab91361fee66b1418cf32833bea
marunemo <chys3697@gmail.com>
2021-12-12 04:20:54 +0900

    feat: login and logout

--- src/main/java/com/localgift/user/LoginController.java
+++ src/main/java/com/localgift/user/LoginController.java
@@ -1,5 +1,7 @@
 package com.localgift.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +16,27 @@ public class LoginController {
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login() {
 		return "login";
+	}
+	
+	@RequestMapping(value="/loginOk", method=RequestMethod.POST)
+	public String loginCheck(HttpSession session, UserVO vo) {
+		if(session.getAttribute("login") != null) {
+			session.removeAttribute("login");
+		}
+		
+		UserVO loginVO = service.getUser(vo);
+		if(loginVO != null) {
+			System.out.println("로그인 성공!");
+			session.setAttribute("login", loginVO);
+			return "redirect:/board/list";
+		}
+		System.out.println("로그인 실패..");
+		return "redirect:login";
+	}
+	
+	@RequestMapping(value="/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:login";
 	}
 }
